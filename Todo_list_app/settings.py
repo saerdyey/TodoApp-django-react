3188d26e5e81e7731cbaf4ac2f3f550eb8b8d4da
jyntn23 <jayantonroblico23@gmail.com>
2020-11-03 23:36:51 +0800

    front end display data

--- Todo_list_app/settings.py
+++ Todo_list_app/settings.py
@@ -41,9 +41,12 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
 
     'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
