b48af4bf0448e7475df7d780fa0efeda875d4fe3
jyntn23 <jayantonroblico23@gmail.com>
2020-11-04 22:17:00 +0800

    frontend display data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      todoList:[],
+      activeItem:{
+        id:null,
+        title:'',
+        completed:false,
+      },
+      editing:false
+    }
+    this.fetchTasks = this.fetchTasks.bind(this)
+  }
+
+  componentWillMount(){
+    this.fetchTasks()
+  }
+
+  fetchTasks(){
+    console.log("Fetching ...")
+
+    fetch("http://localhost:8000/api/task-list/")
+    .then(response => response.json())
+    .then(data =>
+      this.setState({
+        todoList:data
+      })
+    )
+  }
+
+  render(){
+    var tasks = this.state.todoList
+    return(
+      <div id="container">
+
+        <div id="task-container">
+
+          <div id="form-wrapper">
+            <form id="form">
+              <div className="flex-wrapper">
+                <div style={{flex: 6}}>
+                  <input className="form-control" id="title" type="text" name="title" placeholder="Add Task" />
+                </div>
+                <div style={{flex: 1}}>
+                  <input id="submit" className="btn btn-warning" type="submit" name="Add"/>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div id="list-wrapper">
+            {tasks.map((task, index) => {
+              return(
+                <div key={index} className="task-wrapper flex-wrapper">
+                  <div style={{flex: 7}}>
+                    <span>{task.title}</span>
+                  </div>
+                  <div style={{flex: 1}}>
+                    <button className="btn btn-sm btn-outline-info">Edit</button>
+                  </div>
+                  <div style={{flex: 1}}>
+                  <button className="btn btn-sm btn-outline-dark delete">Delete</button>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+
+      </div>
+    )
+  }
 }
 
 export default App;
